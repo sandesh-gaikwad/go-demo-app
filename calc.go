0932b48d1b0ed55d8a4a067144cd78755ac2b861
sandesh-gaikwad <sandesh.gaikwad@afourtech.com>
2020-05-15 13:58:13 +0530

    Go calculator

---
+++ calc.go
@@ -0,0 +1,14 @@
+package main
+
+func Add(x , y int)  int{
+	return x+y
+}
+func Subtract(x , y int)  int{
+	return x-y
+}
+func Multiply(x , y int)  int{
+	return x*y
+}
+func Division(x , y int)  float64{
+	return float64(x/y)
+}
